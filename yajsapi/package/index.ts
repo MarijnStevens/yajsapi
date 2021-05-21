58b43618240ecfaf2496bac7d891fca0bc7391a1
Marijn Stevens <marijnstevens@gmail.com>
2021-05-21 07:49:51 +0200

    Added platform constraint.

--- yajsapi/package/index.ts
+++ yajsapi/package/index.ts
@@ -8,11 +8,17 @@ import { logger } from "../utils";
 const FALLBACK_REPO_URL = "http://yacn2.dev.golem.network:8000";
 export const DEFAULT_REPO_SRV = "_girepo._tcp.dev.golem.network";
 
+export enum Platform {
+  x86_64 = "x86_64",
+  aarch64 = "aarch64"
+}
+
 export type RepoOpts = {
   engine?: string,
   image_hash: string;
   min_mem_gib: number;
   min_storage_gib: number;
+  platform: Platform;
 };
 
 export class Constraints {
--- yajsapi/package/vm.ts
+++ yajsapi/package/vm.ts
@@ -1,11 +1,12 @@
 import { RuntimeType, InfVmKeys } from "../props/inf";
-import { Constraints, DEFAULT_REPO_SRV, Package, RepoOpts, VmPackage, resolve_repo_srv } from ".";
+import { Constraints, Platform, DEFAULT_REPO_SRV, Package, RepoOpts, VmPackage, resolve_repo_srv } from ".";
 
 class _VmConstrains extends Constraints {
   constructor(
     min_mem_gib: number,
     min_storage_gib: number,
-    cores: number = 1
+    platform: Platform,
+    cores: number = 1,
   ) {
     super();
     super.extend([
@@ -13,6 +14,7 @@ class _VmConstrains extends Constraints {
       `(${InfVmKeys["mem"]}>=${min_mem_gib})`,
       `(${InfVmKeys["storage"]}>=${min_storage_gib})`,
       `(${InfVmKeys["runtime"]}=${RuntimeType.VM})`,
+      `(${InfVmKeys["platform"]}=${platform})`,
     ]);
   }
 }
@@ -21,6 +23,7 @@ export async function repo({
   image_hash,
   min_mem_gib = 0.5,
   min_storage_gib = 2.0,
+  platform = Platform.x86_64
 }: RepoOpts): Promise<Package> {
   /*
     Builds reference to a demand decorator.
@@ -34,6 +37,6 @@ export async function repo({
   return new VmPackage({
     repo_url: await resolve_repo_srv({repo_srv: DEFAULT_REPO_SRV}),
     image_hash,
-    constraints: new _VmConstrains(min_mem_gib, min_storage_gib),
+    constraints: new _VmConstrains(min_mem_gib, min_storage_gib, platform),
   });
 }
